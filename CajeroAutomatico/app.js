8437fa929a4a7d2e0ee1127af4d601922576f905
Emerson <emersoncrp@gmail.com>
2022-05-09 23:12:16 -0500

    Este es el cajero Web Magico

---
+++ CajeroAutomatico/app.js
@@ -0,0 +1,74 @@
+const formulario__campo = document.querySelector(".formulario__campo");
+const boton = document.querySelector(".formulario__submit");
+const main = document.querySelector(".main");
+const mostar1 = document.querySelector(".monto1")
+const mostar2 = document.querySelector(".monto2")
+const mostar3 = document.querySelector(".monto3")
+let total = 0;
+const monto = document.querySelector(".monto__ingreso");
+const monto_boton = document.querySelector(".monto__boton-ingreso")
+
+const mostar_retiro = document.querySelector(".monto__retiro");
+const retiro_boton = document.querySelector(".monto__boton-retiro")
+
+const mostar_saldo = document.querySelector(".monto__saldo");
+const mostrar_boton = document.querySelector(".monto__boton-saldo")
+
+const black = document.querySelector(".black_boton");
+
+
+
+monto_boton.addEventListener("click", ingreso);
+retiro_boton.addEventListener("click", retiro);
+
+function ingreso() {
+    total = total + parseFloat(monto.value)
+    return total;
+}
+
+function retiro() {
+    total = total - parseFloat(mostar_retiro.value)
+}
+
+
+mostrar_boton.addEventListener("click", function () {
+    mostar_saldo.value = total;
+})
+
+
+
+boton.addEventListener("click", function () {
+
+    if(formulario__campo.value == ingreso){
+        mostar1.classList.toggle("visibility");
+        mostar2.classList.toggle("visibility");
+
+    }
+
+    switch (formulario__campo.value) {
+
+        case 'ingreso':
+            mostar1.classList.toggle("visibility");
+            mostar2.classList.add("visibility");
+            mostar3.classList.add("visibility");
+
+            break
+
+        case 'retiro':
+            mostar2.classList.toggle("visibility");
+            mostar1.classList.add("visibility");
+            mostar3.classList.add("visibility");
+            break
+
+        case 'saldo':
+            mostar3.classList.toggle("visibility");
+            mostar2.classList.add("visibility");
+            mostar1.classList.add("visibility");
+            break
+
+    }
+})
+
+black.addEventListener("click",function () {
+    main.classList.toggle("black")
+})
